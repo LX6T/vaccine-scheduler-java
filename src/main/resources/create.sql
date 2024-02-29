44ed236407444feaae7e32e9da9ffc94f67051f0
LX6T <alex@pullen.net.net.nz>
2024-02-28 21:50:34 -0800

    Implemented Patient class and patient methods in Scheduler.java

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -5,9 +5,16 @@ CREATE TABLE Caregivers (
     PRIMARY KEY (Username)
 );
 
+CREATE TABLE Patients (
+    Username varchar(255),
+    Salt BINARY(16),
+    Hash BINARY(16),
+    PRIMARY KEY (Username)
+);
+
 CREATE TABLE Availabilities (
     Time date,
-    Username varchar(255) REFERENCES Caregivers,
+    Username varchar(255) NOT NULL REFERENCES Caregivers,
     PRIMARY KEY (Time, Username)
 );
 
@@ -15,4 +22,13 @@ CREATE TABLE Vaccines (
     Name varchar(255),
     Doses int,
     PRIMARY KEY (Name)
+);
+
+CREATE TABLE Appointments (
+    Id int IDENTITY,
+    CaregiverName varchar(255) NOT NULL REFERENCES Caregivers,
+    PatientName varchar(255) NOT NULL REFERENCES Patients,
+    VaccineName varchar(255) NOT NULL REFERENCES Vaccines,
+    Time date,
+    PRIMARY KEY (Id)
 );
---
+++ src/main/resources/delete.sql
@@ -0,0 +1,5 @@
+DROP TABLE Appointments;
+DROP TABLE Vaccines;
+DROP TABLE Availabilities;
+DROP TABLE Patients;
+DROP TABLE Caregivers;
